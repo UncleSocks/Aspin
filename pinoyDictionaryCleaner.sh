f926e2a446386d56b58fa5f85448cd756be49b6d
UncleSocks <ilisan.tyrone@gmail.com>
2024-07-15 00:07:53 +0800

    Update: Improve bash script cleaner and updated the Filipino dictionaries.

--- pinoyDictionaryCleaner.sh
+++ pinoyDictionaryCleaner.sh
@@ -5,27 +5,65 @@ processLine () {
 
         local line=$1
 
-        echo "Processing $line"
-
         if echo "$line" | grep -q ", "; then
-                echo "$line" | cut -d "," -f 1 >> $2
-                echo "$line" | cut -d "," -f 2 | sed 's/^[ \t]*//' >> $2
+
+                wordCounter=1
+                while true; do
+
+                        catchWord=$(echo "$line" | cut -d "," -f $wordCounter | sed 's/^[ \t]*//')
+
+                        if [ -z $catchWord ]; then
+                                break
+                        fi
+
+                        echo "Processing $line"
+                        echo "$catchWord" >> $2
+                        ((wordCounter++))
+
+                done
 
         else
+                echo "Processing $line"
                 echo "$line" >> $2
 
         fi
 }
 
-processedDictionary=$2
+removeDuplicates () {
 
-if [ -f $processedDictionary ]; then
-        chmod 666 $processedDictionary
-else
-        touch $processedDictionary
-        chmod 666 $processedDictionary
-fi
+        echo -e "\nRemoving word duplicates in the dictionary..."
 
-while ISR= read -r line; do
-        processLine "$line" "$processedDictionary"
-done < "$1"
+        echo "Creating a temp file."
+        local tempFile=$(mktemp)
+
+        sort "$1" | uniq > "$tempFile"
+
+        echo "Overwriting temp file..."
+        mv "$tempFile" "$1"
+
+        chmod 666 $1
+
+        echo "Successfully removed all duplicates."
+
+
+}
+
+main () {
+        processedDictionary=$2
+
+        if [ -f $processedDictionary ]; then
+                chmod 666 $processedDictionary
+        else
+                touch $processedDictionary
+                chmod 666 $processedDictionary
+        fi
+
+        while ISR= read -r line; do
+                processLine "$line" "$processedDictionary"
+        done < "$1"
+
+        removeDuplicates "$processedDictionary"
+
+}
+
+main "$@"
