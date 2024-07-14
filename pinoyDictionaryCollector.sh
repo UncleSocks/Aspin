a8e374897ea3f50d710d7393655cc78a3e9abd75
UncleSocks <ilisan.tyrone@gmail.com>
2024-07-15 06:04:06 +0800

    Update: Combined the cleaner Bash script with the collector script.

--- pinoyDictionaryCollector.sh
+++ pinoyDictionaryCollector.sh
@@ -19,29 +19,30 @@ showHelp () {
 
 createDictionary () {
 
-    echo "Checking for $1 dictionary file..."
-    if [ -f ./$1.txt ]; then
-        echo "A $1 dictionary file was found."
-        chmod 666 $1.txt
+    local dialect="$1"
+    local startLetter="$2"
+
+    echo "Checking for $dialect dictionary file..."
+    if [ -f ./$dialect.txt ]; then
+        echo "A $dialect dictionary file was found."
+        chmod 666 $dialect.txt
 
     else
-        echo "Creating a $1 dictionary file..."
-        touch $1.txt
-        echo "Successfully created the $1.txt dictionary file."
-        chmod 666 $1.txt
+        echo "Creating a $dialect dictionary file..."
+        touch $dialect.txt
+        echo "Successfully created the $dialect.txt dictionary file."
+        chmod 666 $dialect.txt
 
     fi
 
     alphabet=('a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z')
 
     startIndex=0
-    if [ -n "$2" ]; then
-
-        findLetter = $2
+    if [ -n "$startLetter" ]; then
 
         for index in "${!alphabet[@]}"; do
             
-            if [ ${alphabet[$index]} == $findLetter ]; then
+            if [ ${alphabet[$index]} == $startLetter ]; then
                 startIndex=$index
                 break
             fi
@@ -54,19 +55,85 @@ createDictionary () {
 
         currentLetter=${alphabet[$currentIndex]}
         pageCounter=1
-        url="https://$1.pinoydictionary.com/list/$currentLetter/"
+        url="https://$dialect.pinoydictionary.com/list/$currentLetter/"
         
         until curl -s "${url}/$pageCounter/" | grep -q "The page you are looking for could not be found."; do
 
             currentPage=$pageCounter
-            echo "Capturing: $1 dictionary on letter '$currentLetter', page '$currentPage'."
+            echo "Capturing: $dialect dictionary on letter '$currentLetter', page '$currentPage'."
 
-            curl -s "${url}/$pageCounter/" | grep h2 | cut -d '>' -f 3 | cut -d '<' -f 1 >> $1.txt
+            curl -s "${url}/$pageCounter/" | grep h2 | cut -d '>' -f 3 | cut -d '<' -f 1 >> $dialect.txt
             ((pageCounter++))
 
         done
     done
 }
+
+
+processLine () {
+
+    local rawDictionary="$2"
+    local line="$1"
+    local tempDictionary="$3"
+
+    if echo "$line" | grep -q ", "; then
+
+        wordCounter=1
+        while true; do
+            
+            catchWord=$(echo "$line" | cut -d "," -f $wordCounter | sed 's/^[ \t]*//')
+
+            if [ -z "$catchWord" ]; then
+                break
+            fi
+
+            echo "Cleaning line: $line"
+            echo "$catchWord" >> "$tempDictionary"
+            ((wordCounter++))
+        done
+
+    else
+        echo "Cleaning line: $line"
+        echo "$line" >> "$tempDictionary"
+    fi
+
+}
+
+
+removeDuplicates () {
+
+    echo -e "\nRemoving word duplicates in the clean dictionary text file..."
+
+    local processedDictionary="$1"
+    local newTempDictionary=$(mktemp)
+
+    sort "$processedDictionary" | uniq > "$newTempDictionary"
+    mv "$newTempDictionary" "$processedDictionary"
+    chmod 666 "$processedDictionary"
+
+    echo "Successfully removed all duplicates."
+
+}
+
+
+dictionaryCleanup () {
+    
+    rawDictionary="$1"
+    echo -e "Initializing dictionary text file cleanup."
+
+    touch "${rawDictionary}Temp.txt"
+    tempDictionary="${rawDictionary}Temp.txt"
+
+    while IFS= read -r line; do
+        processLine "$line" "$rawDictionary" "$tempDictionary"
+    done < "$rawDictionary"
+    echo -e "\nDone writing the clean dictionary text file..."
+
+    mv "$tempDictionary" "$rawDictionary"
+    chmod 666 "$rawDictionary"
+    removeDuplicates "$rawDictionary"
+}
+
 
 main () {
 
@@ -90,6 +157,17 @@ main () {
 
         if [[ $invalidDialect -eq 0 ]]; then
             createDictionary "$@"
+
+            if [ -f "$dialect.txt" ]; then
+
+                rawDictionary="$dialect.txt"
+                dictionaryCleanup "$rawDictionary"
+
+            else
+                echo "The $dialect.txt file cannot be found."
+                exit 1
+            fi
+
         else
             echo "Invalid dialect. The script currently supports tagalog, hiligaynon, and cebuano dialects."
         fi
@@ -97,6 +175,17 @@ main () {
     else
         for dialect in ${dialects[@]}; do
             createDictionary "$dialect"
+
+            if [ -f "$dialect.txt" ]; then
+
+                rawDictionary="$dialect.txt"
+                dictionaryCleanup "$rawDictionary"
+
+            else
+                echo "The $dialect.txt file cannot be found."
+                exit 1
+            fi
+
         done
     fi
 }
