bc49783ad4e67e662e0f9edca21ef63122f3ad5a
UncleSocks <ilisan.tyrone@gmail.com>
2024-07-14 16:34:23 +0800

    Update: Added a Bash script for cleaning up dictionary text files captured by the pinoydictionary-collector.sh

---
+++ pinoydictionary-cleaner.sh
@@ -0,0 +1,30 @@
+# !/bin/bash
+
+processLine () {
+
+        local line=$1
+
+        echo "Processing $line"
+
+        if echo "$line" | grep -q ", "; then
+                echo "$line" | cut -d "," -f 1 >> $2
+                echo "$line" | cut -d "," -f 2 | sed 's/^[ \t]*//' >> $2
+
+        else
+                echo "$line" >> $2
+
+        fi
+}
+
+processedDictionary=$2
+
+if [ -f $processedDictionary ]; then
+        chmod 666 $processedDictionary
+else
+        touch $processedDictionary
+        chmod 666 $processedDictionary
+fi
+
+while ISR= read -r line; do
+        processLine "$line" "$processedDictionary"
+done < "$1"
